f2f46253ce3448e2e877ad8c92b5a648f5f62cf4
Yevhen Tkachenko <joy_123@ukr.net>
2023-05-21 23:46:57 +0200

    app.js started

---
+++ app.js
@@ -0,0 +1,11 @@
+//secect soctions
+const section = document.querySelectorAll('.section');
+//select btn parents element
+const sectBtns = document.querySelector('.controlls');
+//select buttons
+const sectBtn = document.querySelector('.control');
+//select main content
+const allSelections = document.querySelectorAll('.main-content');
+
+
+console.log(section);
