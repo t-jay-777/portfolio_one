d9b67d847ca442d6b551546e86c637336fdc3c78
Yevhen Tkachenko <joy_123@ukr.net>
2023-05-22 23:41:01 +0200

    app.js create a page transition function

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//secect soctions
+//select sections
 const section = document.querySelectorAll('.section');
 //select btn parents element
 const sectBtns = document.querySelector('.controlls');
@@ -9,7 +9,17 @@ const allSelections = document.querySelectorAll('.main-content');
 
 function PageTransitions() {
     //button click on active class
-
+    for (let i = 0; i < sectBtn.length; i+=1) {
+        //catch click on a button and apply a function for the button
+        sectBtn[i].addEventListener('click', () => {
+            //chaice buttons with a class active-btn
+            let currentBtn = document.querySelectorAll('.active-btn');
+            //when I dont click button it replace on empty string
+            currentBtn[0].classList = currentBtn[0].className.replace('active-btn', '');
+            //this refers to addEventListener function
+            this.className += ' active-btn';     
+        });
+    }
 }
 
 console.log(section);
